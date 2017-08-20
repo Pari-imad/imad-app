ef13b8e26ec01679fdf8af8dc359a699a6cb50f7
Pari-imad <31046974+Pari-imad@users.noreply.github.com>
2017-08-21 03:11:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,27 @@
 //Counter code
 var button = document.getElementById('counter');
-var counter = 0;
+
 button.onclick = function () {
-    counter=counter+1
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+    
+    
+    //Create a request object
+    var request = new XMLHttpRequest();
+    
+    //Capture the responce and store it in a variable
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            //Take some action
+            if (request.status === 200){
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+        // Not done Yet
+    };
+    //Render the variable in correct span
+    
+    //Make the request
+    request.open('GET', 'http://paricrp.imad.hasura-app.io/counter',true);
+    request.send(null);
 };
