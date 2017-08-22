f8eaee0b613ff8b3bfc931b43978e4234921feda
Pari-imad <31046974+Pari-imad@users.noreply.github.com>
2017-08-23 00:44:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config = {
+    user:'paricrp',
+    database:'paricrp',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password: process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -9,9 +18,17 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var pool = new Pool(config);
 app.get('/test-db', function (req,res) {
     //Make a select request
     //return a response with the results
+    pool.query('SELECT * FROM test', function (err,result) {
+       if (err) {
+           res.status(500).send(err.toString());
+       } else {
+           res.send(Json.stringify(result));
+       }
+    });
 });
 
 
